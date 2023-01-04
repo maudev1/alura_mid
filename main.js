cfb5facd254f62d6010361aa301532e2d75e1617
mauricio <mauricio@vgt.com.br>
2023-01-04 18:04:28 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// let tecla_pom = document.querySelector('.tecla_pom').onclick = tocaSomPom;
+// let tecla_clap = document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+// function tocaSomPom(){
+//     document.querySelector('#som_tecla_pom').play();
+// }
+// function tocaSomClap(){
+//     document.querySelector('#som_tecla_clap').play();
+
+// }
+
+
+const teclas = document.querySelectorAll('.tecla');
+
+
+teclas.forEach((i) => {
+
+ 
+
+    i.className.split(" ")[1]
+        
+
+})
+
+
