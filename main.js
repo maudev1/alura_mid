c77c57ea43d26877a2ff7b088086f8a2b6ff6f41
mauricio <mauricio@vgt.com.br>
2023-01-05 13:07:31 -0300

    integração com teclado

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-function tocaSom(id_elemento){
-    document.querySelector(id_elemento).play();
+function tocaSom(id_elemento) {
+    const elemento = document.querySelector(id_elemento);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.error('Elemento não encontrado ou inválido!');
+    }
 }
 
 const teclas = document.querySelectorAll('.tecla');
 
-teclas.forEach((element) => {
-    
-    let tecla = element.className.split(" ");
+teclas.forEach((button) => {
 
-    element.onclick = function(){
-        
-        tocaSom(`#som_${tecla[1]}`);
-    } 
-})
+    let nomeTecla = button.className.split(" ")[1];
 
-/**
- * 
- * 
- * ALura
- * 
- */
+    button.onclick = function () {
+        tocaSom(`#som_${nomeTecla}`);
+    }
 
+    button.onkeydown = function (e) {
+        if (e.code === 'Space') {
+            button.classList.add('ativa');
+        }
+    }
+
+    button.onkeyup = function () {
+        button.classList.remove('ativa');
+    }
+});
+
+onkeydown = function (i) {
+    let button = teclas[i.key];
+    button.classList.add('ativa');
+    let nomeTecla = button.className.split(" ")[1];
+    tocaSom(`#som_${nomeTecla}`);
+};
+
+onkeyup = function (i) {
+    let button = teclas[i.key];
+    button.classList.remove('ativa');
+}
